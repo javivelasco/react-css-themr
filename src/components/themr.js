554b5fcfb57fe4ffe02a953f810ce10c2abfb148
Victor Saiz <victor@kognity.com>
2016-08-19 14:38:18 +0200

    no mutations

--- src/components/themr.js
+++ src/components/themr.js
@@ -85,11 +85,11 @@ export default (componentName, localTheme, options = {}) => (ThemedComponent) =>
 function themeable(style = {}, theme) {
   if (!theme) return style
   return Object.keys(theme).reduce((result, key) => (
-    Object.assign({}, result, { [key]:
+    Object.assign(result, { [key]:
        style[key] && theme[key].indexOf(style[key]) === -1
        ? `${style[key]} ${theme[key]}`
        : theme[key] || style[key]
-    })), style)
+    })), Object.assign({}, style))
 }
 
 function validateComposeOption(composeTheme) {

88daa0193da6ff664ab0b6b187a62c1cbe6c0020
Joseph Kohlmann <kohlmannj@gmail.com>
2017-04-29 02:07:33 -0400

    withNamedThemes: `theme` prop can be a 2-item array
    I’ve added support for a `theme` prop like `[‘baseTheme’, themeObjectOrName]`, for ordered usage of a base theme and an override theme. This behavior is intended to respect the `composeTheme` prop.
    
    There’s also a third argument to the `@withNamedThemes()` decorator: a theme object or, more pertinently, the name of the default theme. Previously I was using a string-type defaultProps.theme within a decorated component, but this led to that string being merged with the final theme object passed to the component.

--- src/components/withNamedThemes.js
+++ src/components/withNamedThemes.js
@@ -2,34 +2,70 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import themr from './themr'
 
-const withNamedThemes = (elementName, themesByName) => Element => {
+const getNamedTheme = (themesByName, nameOrTheme) => {
+  let namedTheme
+
+  if (
+    typeof nameOrTheme === 'string' &&
+    typeof themesByName === 'object' &&
+    themesByName !== null &&
+    typeof themesByName[nameOrTheme] === 'object' &&
+    themesByName[nameOrTheme] !== null
+  ) {
+    namedTheme = themesByName[nameOrTheme]
+  } else if (typeof nameOrTheme === 'object' && nameOrTheme !== null) {
+    namedTheme = nameOrTheme
+  }
+
+  return namedTheme
+}
+
+const isThemeProp = themeProp =>
+  typeof themeProp === 'string' ||
+  (typeof themeProp === 'object' && themeProp !== null)
+
+const withNamedThemes = (elementName, themesByName, defaultThemeName) => Element => {
   const ThemeableElementDisplayName = `Themed${elementName || Element.displayName || 'Element'}WithNamedThemes`
 
   const NamedThemeElementWrapper = ({ theme, ...rest }) => {
-    let namedTheme
-    const themeProp = theme || Element.defaultProps.theme
+    let firstThemeProp
+    let secondThemeProp
 
-    if (
-      typeof themeProp === 'string' &&
-      typeof themesByName === 'object' &&
-      themesByName !== null &&
-      typeof themesByName[themeProp] === 'object' &&
-      themesByName[themeProp] !== null
-    ) {
-      namedTheme = themesByName[themeProp]
-    } else if (typeof themeProp === 'object' && themeProp !== null) {
-      namedTheme = themeProp
+    if (Array.isArray(theme) === true && theme.length >= 1) {
+      if (isThemeProp(theme[0]) === true) {
+        firstThemeProp = theme[0]
+      }
+
+      if (isThemeProp(theme[1]) === true) {
+        secondThemeProp = theme[1]
+      }
+    } else {
+      const possibleFirstThemeProp = theme || defaultThemeName || Element.defaultProps.theme
+      if (isThemeProp(possibleFirstThemeProp) === true) {
+        firstThemeProp = possibleFirstThemeProp
+      }
     }
 
-    const ThemedElement = themr(ThemeableElementDisplayName, namedTheme)(Element)
+    const firstTheme = getNamedTheme(themesByName, firstThemeProp)
+    const secondTheme = getNamedTheme(themesByName, secondThemeProp)
 
-    return <ThemedElement {...rest} />
+    const ThemedElement = themr(ThemeableElementDisplayName, firstTheme)(Element)
+    const themedElementProps = {
+      ...rest,
+      theme: secondTheme
+    }
+
+    return <ThemedElement {...themedElementProps} />
   }
 
   NamedThemeElementWrapper.propTypes = {
     theme: PropTypes.oneOfType([
-      PropTypes.string,
-      PropTypes.object // eslint-disable-line react/forbid-prop-types
+      PropTypes.arrayOf(PropTypes.oneOfType([
+        PropTypes.object, // eslint-disable-line react/forbid-prop-types
+        PropTypes.string
+      ])),
+      PropTypes.object, // eslint-disable-line react/forbid-prop-types
+      PropTypes.string
     ])
   }
 

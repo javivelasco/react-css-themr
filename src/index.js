d15bdaae054e73abed8d4489ddce8a0ebf86833f
Joseph Kohlmann <kohlmannj@gmail.com>
2017-04-28 21:25:35 -0400

    Export withNamedThemes from package index

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export { default as ThemeProvider } from './components/ThemeProvider'
 export { default as themr } from './components/themr'
+export { default as withNamedThemes } from './components/withNamedThemes'
 export { themeable } from './components/themr'

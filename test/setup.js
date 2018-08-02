fef61dfa19a05a016c6edb0009166e96d31faf35
Markus Günther <markus.guenther@noerdisch.de>
2018-08-02 09:36:53 +0200

    TASK: Use polyfill to suppress ReactAnimationFrame warning

--- test/setup.js
+++ test/setup.js
@@ -1,6 +1,6 @@
+import raf from './tempPolyfills'
 import { jsdom } from 'jsdom'
 
-process.env.NODE_ENV = 'TESTING'
 global.document = jsdom('<!doctype html><html><body></body></html>')
 global.window = document.defaultView
 global.navigator = global.window.navigator
